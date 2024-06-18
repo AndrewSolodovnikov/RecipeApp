170c391b8ce574f58b8e33a8514408279096c3f2
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-06-18 23:33:01 +0300

    RA-03 - fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,9 +39,8 @@ android {
 }
 
 dependencies {
-    val fragment_version = "1.8.0"
 
-    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+    implementation(libs.fragment)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
