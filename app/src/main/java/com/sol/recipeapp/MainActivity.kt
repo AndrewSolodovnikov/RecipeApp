5fd3768a4699366e08a2d234bddeb07da05c3439
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-06-20 22:41:26 +0300

    RA-04 - add fragment

--- app/src/main/java/com/sol/recipeapp/MainActivity.kt
+++ app/src/main/java/com/sol/recipeapp/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
 import com.sol.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val fragmentManager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.mainContainer, CategoriesListFragment()).commit()
     }
 }
