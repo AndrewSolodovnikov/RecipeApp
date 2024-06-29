265dc81749fa21d25759cabf32605e6ce29070fc
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-06-29 16:09:31 +0300

    RA-07

--- app/src/main/java/com/sol/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/sol/recipeapp/CategoriesListFragment.kt
@@ -1,11 +1,13 @@
 package com.sol.recipeapp
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.sol.recipeapp.databinding.FragmentListCategoriesBinding
+import java.io.IOException
 
 class CategoriesListFragment : Fragment() {
     private var _binding: FragmentListCategoriesBinding? = null
@@ -18,6 +20,21 @@ class CategoriesListFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        placeHeaderImage()
+
         return view
+    }
+
+    fun placeHeaderImage() {
+        val assetManager = requireContext().assets
+        try {
+            val inputStream = assetManager.open("bcg_categories.png")
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            binding.categoryHeaderImage.setImageBitmap(bitmap)
+            inputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 }
