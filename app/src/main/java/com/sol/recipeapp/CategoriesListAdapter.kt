7c24ef6e2a1b2f120af44a4ac01808ef5b897239
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-07-04 22:59:59 +0300

    RA-08

--- app/src/main/java/com/sol/recipeapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/sol/recipeapp/CategoriesListAdapter.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.sol.recipeapp.databinding.ItemCategoryBinding
-import java.io.IOException
 import java.io.InputStream
 
 
