e4e96d515cb3899c89412cfef88f32dfb9a0dd9e
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-07-11 22:47:09 +0300

    RA-09 fixed

--- app/src/main/java/com/sol/recipeapp/RecipesListFragment.kt
+++ app/src/main/java/com/sol/recipeapp/RecipesListFragment.kt
@@ -8,14 +8,12 @@ import androidx.fragment.app.Fragment
 import com.sol.recipeapp.databinding.FragmentRecipesListBinding
 
 class RecipesListFragment : Fragment() {
-    private var _binding: FragmentRecipesListBinding? = null
-    private val binding get() = _binding!!
+    private val binding by lazy { FragmentRecipesListBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentRecipesListBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
