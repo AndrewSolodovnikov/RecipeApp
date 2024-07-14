08b98b9cc6f633a2036403fed0edf433298514b9
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-07-14 15:09:48 +0300

    RA-11 WIP

--- app/src/main/java/com/sol/recipeapp/RecipesListFragment.kt
+++ app/src/main/java/com/sol/recipeapp/RecipesListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sol.recipeapp.databinding.FragmentRecipesListBinding
 
 class RecipesListFragment : Fragment() {
@@ -26,6 +28,17 @@ class RecipesListFragment : Fragment() {
             categoryName = it?.getString(ARG_CATEGORY_NAME)
             categoryImageUrl = it?.getString(ARG_CATEGORY_IMAGE_URL)
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initRecycler()
+    }
+
+    private fun initRecycler() {
+        val customAdapter = CategoriesListAdapter(STUB.getCategories())
+
+        binding.rvCategory.layoutManager = LinearLayoutManager(context)
+        binding.rvCategory.adapter = customAdapter
     }
 
 }
