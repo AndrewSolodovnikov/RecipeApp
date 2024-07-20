813239cf76034dc863d2a02c1d6221798ce7f36c
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-07-20 15:13:04 +0300

    RA-12

--- app/src/main/java/com/sol/recipeapp/RecipeFragment.kt
+++ app/src/main/java/com/sol/recipeapp/RecipeFragment.kt
@@ -1,5 +1,6 @@
 package com.sol.recipeapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,15 +8,28 @@ import android.view.View
 import android.view.ViewGroup
 import com.sol.recipeapp.databinding.FragmentRecipeBinding
 
+
 class RecipeFragment : Fragment() {
     private val binding by lazy { FragmentRecipeBinding.inflate(layoutInflater) }
+    private var recipe: Recipe? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arguments?.getParcelable(ARG_RECIPE, Recipe::class.java)
+        } else {
+            arguments?.getParcelable(ARG_RECIPE)
+        }
+
+        binding.recipeTitleText.text = recipe?.title
     }
 }
