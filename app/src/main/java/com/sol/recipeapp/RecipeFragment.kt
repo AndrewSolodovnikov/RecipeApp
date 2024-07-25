320567e23f1a81a1caab30a1bb1be57be1e135fc
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-07-25 23:34:13 +0300

    RA-13

--- app/src/main/java/com/sol/recipeapp/RecipeFragment.kt
+++ app/src/main/java/com/sol/recipeapp/RecipeFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.sol.recipeapp.databinding.FragmentRecipeBinding
 
 
@@ -29,6 +31,22 @@ class RecipeFragment : Fragment() {
             arguments?.getParcelable(ARG_RECIPE)
         }
 
-        binding.recipeTitleText.text = recipe?.title
+        initRecycler()
+    }
+
+    private fun initRecycler() {
+        binding.rvIngredients.adapter = recipe?.let { IngredientsAdapter(it.ingredients) }
+        binding.rvIngredients.layoutManager = LinearLayoutManager(context)
+
+        val divider = MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
+        divider.dividerThickness = 2
+        binding.rvIngredients.addItemDecoration(divider)
+        binding.rvMethod.addItemDecoration(divider)
+
+        binding.rvIngredients.adapter = recipe?.let { IngredientsAdapter(it.ingredients) }
+        binding.rvIngredients.layoutManager = LinearLayoutManager(context)
+
+        binding.rvMethod.adapter = recipe?.let { MethodAdapter(it.method) }
+        binding.rvMethod.layoutManager = LinearLayoutManager(context)
     }
 }
