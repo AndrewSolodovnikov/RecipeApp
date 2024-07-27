e25ba86692d6bd25838bfd782ba02c0b60d13454
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-07-27 22:41:08 +0300

    RA-13 fixed

--- app/src/main/java/com/sol/recipeapp/RecipeFragment.kt
+++ app/src/main/java/com/sol/recipeapp/RecipeFragment.kt
@@ -1,7 +1,9 @@
 package com.sol.recipeapp
 
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +11,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.sol.recipeapp.databinding.FragmentRecipeBinding
-
+import java.io.InputStream
 
 class RecipeFragment : Fragment() {
     private val binding by lazy { FragmentRecipeBinding.inflate(layoutInflater) }
@@ -32,13 +34,32 @@ class RecipeFragment : Fragment() {
         }
 
         initRecycler()
+        loadHeader()
+    }
+
+    private fun loadHeader() {
+        try {
+            val inputStream: InputStream? = recipe?.imageUrl?.let { context?.assets?.open(it) }
+            val drawable = Drawable.createFromStream(inputStream, null)
+            binding.ivRecipesHeaderImage.setImageDrawable(drawable)
+        } catch (e: Exception) {
+            Log.e("MyLogError", "Image ${recipe?.imageUrl} not found")
+        }
+
+        binding.tvRecipesHeaderTitle.text = recipe?.title
     }
 
     private fun initRecycler() {
         binding.rvIngredients.adapter = recipe?.let { IngredientsAdapter(it.ingredients) }
         binding.rvIngredients.layoutManager = LinearLayoutManager(context)
 
-        val divider = MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
+        val divider =
+            MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL).apply {
+                isLastItemDecorated = false
+                dividerInsetStart = resources.getDimensionPixelSize(R.dimen.padding_text_12)
+                dividerInsetEnd = resources.getDimensionPixelSize(R.dimen.padding_text_12)
+                dividerColor = resources.getColor(R.color.divider_color)
+            }
         divider.dividerThickness = 2
         binding.rvIngredients.addItemDecoration(divider)
         binding.rvMethod.addItemDecoration(divider)
