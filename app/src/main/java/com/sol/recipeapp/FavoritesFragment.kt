3c722e2d0971f2ce6ed3fafe49231655f5693c26
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-08-08 22:10:49 +0300

    RA-18 wip

--- app/src/main/java/com/sol/recipeapp/FavoritesFragment.kt
+++ app/src/main/java/com/sol/recipeapp/FavoritesFragment.kt
@@ -8,16 +8,12 @@ import android.view.ViewGroup
 import com.sol.recipeapp.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
-    private var _binding: FragmentFavoritesBinding? = null
-    private val binding get() = _binding!!
+    private val binding by lazy { FragmentFavoritesBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
     }
 }
