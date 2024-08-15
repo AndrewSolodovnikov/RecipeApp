b95e8aae00ef066e8ce027a1bdfc8ad6ae2f8fb2
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-08-15 19:07:04 +0300

    RA-20 fixed

--- app/src/main/java/com/sol/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/sol/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -3,11 +3,13 @@ package com.sol.recipeapp.ui.recipes.recipe
 import android.os.Parcelable
 import androidx.lifecycle.ViewModel
 import com.sol.recipeapp.data.Ingredient
+import com.sol.recipeapp.data.Recipe
 import kotlinx.parcelize.Parcelize
 
 class RecipeViewModel : ViewModel() {
 
     data class RecipeState(
+        val recipe: Recipe? = null,
         val portionCount: Int = 1,
         val isFavorite: Boolean = false,
     )
