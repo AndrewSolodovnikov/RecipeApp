4d7e7f4af19f3962307f4b1aecdef7ab2da9d9d9
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-10-12 21:58:08 +0300

    RA-28 fixed

--- app/src/main/java/com/sol/recipeapp/MainActivity.kt
+++ app/src/main/java/com/sol/recipeapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.sol.recipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import com.sol.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val navOptions = NavOptions.Builder().setLaunchSingleTop(true).build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +17,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.navigationButtonCategory.setOnClickListener {
-            findNavController(R.id.nav_host_fragment_container).navigate(R.id.categoriesListFragment)
+            findNavController(R.id.nav_host_fragment_container)
+                .navigate(R.id.categoriesListFragment, null, navOptions)
         }
 
         binding.navigationButtonFavorites.setOnClickListener {
-            findNavController(R.id.nav_host_fragment_container).navigate(R.id.favoritesFragment)
+            findNavController(R.id.nav_host_fragment_container)
+                .navigate(R.id.favoritesFragment, null, navOptions)
         }
     }
 }
